ea58faf724c807e6356f85951e3a0022df89af52
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2023-08-16 21:46:48 +0100

    Added GitHub link + Github footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was coded by Brooke Clarke and is 
+        <a href='https://github.com/BrookeClarke/weather-app--react-project' target="_blank">open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
