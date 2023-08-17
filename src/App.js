313f27c8af4e0bd650f76c21efdded086ce75ded
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2023-08-17 20:14:58 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       Weather App
       <footer>
         This project was coded by Brooke Clarke and is {" "}
         <a href='https://github.com/BrookeClarke/weather-app--react-project' target="_blank">open-sourced on GitHub</a>
-      </footer>
+        </footer>
+      </div>
     </div>
   );
 }
