fcffc8ea5e61c8454e1d89f8b9c0559b1725e5da
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2023-08-27 18:12:35 +0100

    Done the Animated Weather Icons

--- src/Weather.js
+++ src/Weather.js
@@ -13,7 +13,7 @@ export default function Weather(props) {
             ready: true,
             date: new Date(response.data.dt * 1000),
             city: response.data.name,
-            icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+            icon: response.data.weather[0].icon,
             description: response.data.weather[0].description,
             temperature: response.data.main.temp,
             humidity: response.data.main.humidity,
